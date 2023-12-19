bf779d66377e39bfc653de99da2423fb1e2b9c71
Emanuel Ramos <ramosg2e@gmaill.com>
2023-12-19 16:48:43 -0300

    deu merda no quiz

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        question: "Qual é a capital da Nigéria?",
+        answers: [
+            { option: "Abuja", correct: true},
+            { option: "Cidade do Cabo", correct: false},
+            { option: "Luanda", correct: false},
+        ],
+
+        },
+    {
+        question: "Qual é o país mais populoso do mundo?",
+        answers: [
+            { option: "China", correct: false},
+            { option: "Indía", correct: true},
+            { option: "Paquistão", correct: false},
+        ],
+        },  
+    {
+        question: "Quantos continentes existem?",
+        answers: [
+            { option: "5", correct: false},
+            { option: "6", correct: true},
+            { option: "4", correct: false},
+        ],
+        },
+    {
+        question: "Qual é o idioma oficiial da Suíça?",
+        answers: [
+            { option: "Alemão, Francês, Italiano e Suíço" },
+            { option: "Alemão, Francês e Italiano, Romanche" },
+            { option: "Alemão, Francês e Suíço" },
+        ],
+        },      
+    
+]
